a75dcab5e7fbb10da5ea97f285a0698f396fc61f
Ivonne Mtz <Ivonnemtz96@outlook.es>
2023-04-10 17:00:20 -0700

    falta bg de servicios y la info

---
+++ modules/servicios.php
@@ -0,0 +1,110 @@
+<div class="breadcumb-wrapper" data-bg-src="/assets/img/breadcumb/breadcumb-bg-2.jpg?=1">
+    <div class="container z-index-common">
+        <div class="breadcumb-content">
+            <h1 class="breadcumb-title">
+                Nuestros <span class="inner-text">Servicios</span>
+            </h1>
+            <div class="breadcumb-menu-wrap">
+                <ul class="breadcumb-menu">
+                    <li><a href="/">Inicio</a></li>
+                    <li><span class="inner-text">Servicios</span></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<section class="space-top space-extra-bottom" data-bg-src="/assets/img/bg/price-bg-3-1.png?=1">
+    <div class="container">
+        <div class="title-area text-center wow fadeInUp" data-wow-delay="0.2s">
+            <span class="sec-subtitle">andfaison pricing</span>
+            <h2 class="sec-title">Our Exclusive Plan</h2>
+            <div class="sec-shape">
+                <img src="/assets/img/shape/sec-shape-1.png" alt="shape" />
+            </div>
+        </div>
+        <div class="row vs-carousel wow fadeInUp" data-wow-delay="0.3s" data-slide-show="3" data-lg-slide-show="2"
+            data-md-slide-show="2">
+            <div class="col-xl-4">
+                <div class="package-style1">
+                    <div class="package-top">
+                        <div class="package-left">
+                            <p class="package-price">12<span class="currency">$</span></p>
+                            <p class="package-duration">Billed Monthly</p>
+                        </div>
+                        <h3 class="package-name">Basic Plan</h3>
+                    </div>
+                    <div class="package-shape">
+                        <img src="/assets/img/shape/price-shape-2.png" alt="shape" />
+                    </div>
+                    <div class="package-list">
+                        <ul class="list-unstyled">
+                            <li><span class="text-title">Mobile-Optimized</span></li>
+                            <li>Free Custom Domain</li>
+                            <li>Best Hosting</li>
+                            <li>Max Makup</li>
+                            <li>Outstanding Support</li>
+                            <li>Happy Customers</li>
+                        </ul>
+                    </div>
+                    <div class="package-btn">
+                        <a href="contact.html" class="vs-btn style3">Book Now</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4">
+                <div class="package-style1">
+                    <div class="package-top">
+                        <div class="package-left">
+                            <p class="package-price">29<span class="currency">$</span></p>
+                            <p class="package-duration">Billed Monthly</p>
+                        </div>
+                        <h3 class="package-name">Mega Plan</h3>
+                    </div>
+                    <div class="package-shape">
+                        <img src="/assets/img/shape/price-shape-2.png" alt="shape" />
+                    </div>
+                    <div class="package-list">
+                        <ul class="list-unstyled">
+                            <li><span class="text-title">Mobile-Optimized</span></li>
+                            <li>Free Custom Domain</li>
+                            <li>Best Hosting</li>
+                            <li>Max Makup</li>
+                            <li>Outstanding Support</li>
+                            <li>Happy Customers</li>
+                        </ul>
+                    </div>
+                    <div class="package-btn">
+                        <a href="contact.html" class="vs-btn style3">Book Now</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4">
+                <div class="package-style1">
+                    <div class="package-top">
+                        <div class="package-left">
+                            <p class="package-price">45<span class="currency">$</span></p>
+                            <p class="package-duration">Billed Yearly</p>
+                        </div>
+                        <h3 class="package-name">VIP Plan</h3>
+                    </div>
+                    <div class="package-shape">
+                        <img src="/assets/img/shape/price-shape-2.png" alt="shape" />
+                    </div>
+                    <div class="package-list">
+                        <ul class="list-unstyled">
+                            <li><span class="text-title">Mobile-Optimized</span></li>
+                            <li>Free Custom Domain</li>
+                            <li>Best Hosting</li>
+                            <li>Max Makup</li>
+                            <li>Outstanding Support</li>
+                            <li>Happy Customers</li>
+                        </ul>
+                    </div>
+                    <div class="package-btn">
+                        <a href="contact.html" class="vs-btn style3">Book Now</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ servicios.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+$title = "Inicio";
+include('includes/head.php');
+?>
+
+<body>
+    <?
+    include('includes/preloader.php');
+    include('includes/header.php');
+    include('modules/servicios.php');
+    include('includes/footer.php');
+    include('includes/scripts.php');
+    ?>
+</body>
+
+</html>
